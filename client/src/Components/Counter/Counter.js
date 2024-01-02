9cc21a8543858c23c8ca0134b605ff23429660c5
Kapil <kapilm@advantageclub.in>
2024-01-02 22:42:44 +0530

    details:kapil

--- client/src/Components/Counter/Counter.js
+++ client/src/Components/Counter/Counter.js
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-
-const Counter = ({plus,substract}) => {
+const Counter = ({ plus, substract, quantity, isSelectedSize }) => {
   const [count, setCount] = useState(0);
 
   const increment = () => {
+    quantity?.map((ele) => {
+      if (isSelectedSize == ele.size) {
+        console.log(ele.quantity);
+        if (count > ele.quantity) {
+          return alert("exceeded");
+        }
+      }
+    });
+    
     setCount(count + 1);
   };
 
   const decrement = () => {
-    if(count > 0)
-    setCount(count - 1);
+    if (count > 0) setCount(count - 1);
   };
 
   return (
-    <div className='flex gap-6'>
-      <button onClick={decrement}><img src={substract}/></button>
+    <div className="flex gap-6">
+      <button onClick={decrement}>
+        <img src={substract} />
+      </button>
       <h1>{count}</h1>
-      <button onClick={increment}><img src={plus}/></button>
+      <button onClick={increment}>
+        <img src={plus} />
+      </button>
     </div>
   );
 };
