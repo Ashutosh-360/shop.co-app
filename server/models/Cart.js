ae842b31d1a6e5724a813349c124b6de1dbf1426
Ashutosh Naik <ashutoshn@advantageclub.co>
2024-01-12 23:25:06 +0530

    cart

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,11 +1,32 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema(
-    {
+const sizesValues = ["XS", "S", "M", "L", "XL", "XXL"];
+
+const productSchema = new mongoose.Schema({
+  product_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+    enum: sizesValues,
+    trim: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const cartSchema = new mongoose.Schema({
+  user_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
-    
+  },
+  products: [productSchema],
 });
 
 const cart = mongoose.model("Cart", cartSchema);
