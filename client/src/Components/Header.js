459951e7aa41ef1af034e241cd0ab5c75b928a10
Kapil <kapilmadankar80@gmail.com>
2024-02-11 22:47:17 +0530

    profile pages:kapil

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -87,7 +87,7 @@ function Header() {
                 </a>
                
                 <a
-                  href="#"
+                  href="edit_profile"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
                   Edit Profile
